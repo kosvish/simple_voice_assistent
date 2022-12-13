3af80abeff51088eeb7668b3bdb3dbde48b4f3a3
kosvish <112478497+kosvish@users.noreply.github.com>
2022-12-13 16:08:19 +0300

    Add a Recording Function and Speech Recognition

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import speech_recognition
 
+
+def record_and_recognize_audio(*args: tuple):
+    """
+    Запись и распознование аудио
+    """
+    with microphone:
+        recognized_data = ""
+
+        # регулирование уровня шума окружаюшего мира
+        recognizer.adjust_for_ambient_noise(microphone, duration=2)
+
+        try:
+            print('Слушаю...')
+            audio = recognizer.listen(microphone, 5, 5)
+
+        except speech_recognition.WaitTimeoutError:
+            pass
+
+        # Использование онлайн-распознание через Гугл
+        try:
+            print('Распознование началось...')
+            recognized_data = recognizer.recognize_google(audio, language='ru').lower()
+
+        except speech_recognition.UnknownValueError:
+            print('Проверьте пожалуйта ваше соединение с интернетом')
+
+        return recognized_data
+
+
 if __name__ == "__main__":
 
     # инициализация инструментов распознавания и ввода речи
