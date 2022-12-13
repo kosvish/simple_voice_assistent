15d343594fedffb39dc054e625892d5955811728
kosvish <112478497+kosvish@users.noreply.github.com>
2022-12-13 17:05:45 +0300

    Creating a VoiceAssistant class, adding the created wav file to main if

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import pyttsx3
 import speech_recognition
 import wave  # Создание и чтение аудиофайлов формата wav
 import json  # работа с json файлами и строками
 import os  # Работа с файловой системой
+
+
+class VoiceAssistant:
+    """
+    Настройки голосового ассистента
+    """
+    name = ''
+    sex = ''
+    speech_language = ''
+    recognition_language = ''
 
 
 def record_and_recognize_audio(*args: tuple):
@@ -42,10 +53,27 @@ if __name__ == "__main__":
     recognizer = speech_recognition.Recognizer()
     microphone = speech_recognition.Microphone()
 
+    # инициализация инструмента синтеза речи
+    ttsEngine = pyttsx3.init()
+
+    # настройка данных голосового помощника
+    assistant = VoiceAssistant()
+    assistant.name = 'Alice'
+    assistant.sex = 'female'
+    assistant.speech_language = 'ru'
+
     while True:
         # старт записи речи с последующим выводом распознанной речи
         voice_input = record_and_recognize_audio()
         # Удаление записанного в микрофон аудио
         os.remove('microphone-results.wav')
         print(voice_input)
+
+        # отделение команд от дополнительной информации (аргументов)
+        voice_input = voice_input.split(' ')
+        command = voice_input[0]
+
+        # Тестовый запрос
+        if command == 'привет':
+            play_voice_assistant_speech('Здравствуй')
 
